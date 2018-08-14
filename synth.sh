b4140a14639a1a68d0e33e8a813dd4f1035d077d
ChangHwan Lee <a3626a@kaist.ac.kr>
2018-08-14 14:16:25 +0900

    Untrack script

--- synth.sh
+++
@@ -1,8 +0,0 @@
-
-python synthesis.py \
-       checkpoints/checkpoint_step000410000_ema.pth \
-       generated/eval \
-       --preset=presets/seoul_corpus.json \
-       --conditional="../tacotron2/tacotron_output/eval/speech-mel-00000(mel-tar).npy" \
-       --speaker-id=113 \
-       --max-abs-value=4.0
